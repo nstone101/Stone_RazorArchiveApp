9c0c13788d3c7b883506e4e51de73c8bef47a4d4
Nick Stone <nstone101@gmail.com>
2023-03-03 04:24:08 -0600

    Initial Project complete commit -- now sharpen it up uploader and formatting.

---
+++ RazorMvcMultiUpload/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorMvcMultiUpload.Models;
+
+namespace RazorMvcMultiUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit = 409715200)]
+        [RequestSizeLimit(409715200)]
+        [Obsolete]
+        public IActionResult Upload(IFormFile file, [FromServices] IHostingEnvironment ohostingEnvironment)
+        {
+            string fileName = $"{ohostingEnvironment.WebRootPath}\\UploadedFiles\\{file.FileName}";
+
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+
+            ViewData["message"] = $"File uploaded Successful. File Length : (file.Length) bytes";
+
+
+            return View("Index");
+
+            
+        }
+
+      
+    }
+}
---
+++ RazorMvcMultiUpload/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RazorMvcMultiUpload.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
